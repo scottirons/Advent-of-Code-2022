8dab28dd750c3b4b4dd56f4f00cbd150f7f1640c
Scott Irons <87613441+scottirons@users.noreply.github.com>
2022-12-01 14:27:38 -0500

    done in python as well

--- Python/day_1.py
+++ Python/day_1.py
@@ -1,3 +1,26 @@
 def solve():
-    with open("../../inputs/day_1.txt", "r") as infile:
-        pass
+    with open("../inputs/day_1.txt", "r") as infile:
+        lines = infile.readlines()
+
+    third, second, first = 0, 0, 0
+    running_sum = 0
+    for line in lines:
+        if line == "\n":
+            if running_sum >= first:
+                third = second
+                second = first
+                first = running_sum
+            elif running_sum >= second:
+                third = second
+                second = running_sum
+            elif running_sum >= third:
+                third = running_sum
+            running_sum = 0
+        else:
+            running_sum += int(line[:-1])
+
+    print(f'Greatest: {first} \nSum of three greatest: {first + second + third}\n')
+
+
+if __name__ == "__main__":
+    solve()
