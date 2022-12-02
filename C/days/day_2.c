bdecddcf91de4a1d4f6b641d96b68c4b34c55a19
Scott Irons <87613441+scottirons@users.noreply.github.com>
2022-12-02 08:07:48 -0500

    initialized docs for C day 2

---
+++ C/days/day_2.c
@@ -0,0 +1,9 @@
+//
+// Created by Scott Irons on 12/2/22.
+//
+
+#include "day_2.h"
+#include <string.h>
+#include <stdlib.h>
+
+void day_2() {}
---
+++ C/days/day_2.h
@@ -0,0 +1,10 @@
+//
+// Created by Scott Irons on 12/2/22.
+//
+
+#ifndef C_DAY_2_H
+#define C_DAY_2_H
+
+void day_2();
+
+#endif //C_DAY_2_H
