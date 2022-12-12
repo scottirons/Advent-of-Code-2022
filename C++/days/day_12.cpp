daeccc86d3c186e082f1c8bce2548277e41f9b97
Scott Irons <87613441+scottirons@users.noreply.github.com>
2022-12-12 10:24:22 -0500

    updated day 12

---
+++ C++/days/day_12.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Scott Irons on 12/12/22.
+//
+
+#include "day_12.h"
---
+++ C++/days/day_12.h
@@ -0,0 +1,8 @@
+//
+// Created by Scott Irons on 12/12/22.
+//
+
+#ifndef C_DAY_12_H
+#define C_DAY_12_H
+
+#endif //C_DAY_12_H
