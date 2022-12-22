c2a10afd98bc6abfb2a81191f18406f57d1ca0c0
Scott Irons <87613441+scottirons@users.noreply.github.com>
2022-12-22 16:10:35 -0500

    day 22 done

--- Rust/src/day_22.rs
+++ Rust/src/day_22.rs
@@ -124,6 +124,7 @@ pub fn solution() {
         }
     }
     part_a(&instructions, &rows, &cols, &grid);
+    part_b(&instructions, &rows, &cols, &grid);
 }
 
 fn part_a(instructions: &Vec<Instruction>, rows: &Vec<Dimension>, cols: &Vec<Dimension>, grid: &Vec<Vec<char>>) {
@@ -146,7 +147,7 @@ fn part_a(instructions: &Vec<Instruction>, rows: &Vec<Dimension>, cols: &Vec<Dim
             _ => ()
         }
     }
-    println!("{}", (curr_loc.0 + 1) * 1000 + (curr_loc.1 + 1) * 4 + curr_dir as usize);
+    println!("Part A: {}", (curr_loc.0 + 1) * 1000 + (curr_loc.1 + 1) * 4 + curr_dir as usize);
 }
 
 fn move_row(grid: &Vec<Vec<char>>, rows: &Vec<Dimension>, start: &(usize, usize), num: &usize, dir: &i8) -> (usize, usize) {
@@ -211,14 +212,166 @@ fn move_col(grid: &Vec<Vec<char>>, cols: &Vec<Dimension>, start: &(usize, usize)
     (row, start.1)
 }
 
+// okayyyy, let's go
 fn part_b(instructions: &Vec<Instruction>, rows: &Vec<Dimension>, cols: &Vec<Dimension>, grid: &Vec<Vec<char>>) {
 
+    let mut curr_loc: (usize, usize) = (0, rows[0].start);
+    // 0: R, 1: D, 2: L, 3: U
+    let mut curr_dir: i8 = 0;
+
+    for instruction in instructions {
+        let mut moves = instruction.moves;
+        while moves > 0 {
+            moves -= 1;
+            match curr_dir {
+                0 => {
+                    if curr_loc.1 < rows[curr_loc.0].end {
+                        if grid[curr_loc.0][curr_loc.1 + 1] != '#' {
+                            curr_loc.1 += 1;
+                        } else {
+                            moves = 0;          // sketchy break statement???
+                        }
+                    } else {
+                        let (next_loc, next_dir) = cube_stuff(grid, &curr_loc, &curr_dir,
+                        rows, cols);
+                        if grid[next_loc.0][next_loc.1] != '#' {
+                            curr_loc = next_loc;
+                            curr_dir = next_dir;
+                        }
+                    }
+                }
+                2 => {
+                    if curr_loc.1 >= rows[curr_loc.0].start + 1 {
+                        if grid[curr_loc.0][curr_loc.1 - 1] != '#' {
+                            curr_loc.1 -= 1;
+                        } else {
+                            moves = 0;          // sketchy break statement???
+                        }
+                    } else {
+                        let (next_loc, next_dir) = cube_stuff(grid, &curr_loc, &curr_dir,
+                                                              rows, cols);
+                        if grid[next_loc.0][next_loc.1] != '#' {
+                            curr_loc = next_loc;
+                            curr_dir = next_dir;
+                        }
+                    }
+                }
+                1 => {
+                    if curr_loc.0 < cols[curr_loc.1].end {
+                        if grid[curr_loc.0 + 1][curr_loc.1] != '#' {
+                            curr_loc.0 += 1;
+                        } else {
+                            moves = 0;          // sketchy break statement???
+                        }
+                    } else {
+                        let (next_loc, next_dir) = cube_stuff(grid, &curr_loc, &curr_dir,
+                                                              rows, cols);
+                        if grid[next_loc.0][next_loc.1] != '#' {
+                            curr_loc = next_loc;
+                            curr_dir = next_dir;
+                        }
+                    }
+                }
+                3 => {
+                    if curr_loc.0 >= cols[curr_loc.1].start + 1 {
+                        if grid[curr_loc.0 - 1][curr_loc.1] != '#' {
+                            curr_loc.0 -= 1;
+                        } else {
+                            moves = 0;          // sketchy break statement???
+                        }
+                    } else {
+                        let (next_loc, next_dir) = cube_stuff(grid, &curr_loc, &curr_dir,
+                                                              rows, cols);
+                        if grid[next_loc.0][next_loc.1] != '#' {
+                            curr_loc = next_loc;
+                            curr_dir = next_dir;
+                        }
+                    }
+                }
+                _ => panic!()
+            }
+        }
+        // update next direction
+        match instruction.turn {
+            Turn::L => curr_dir = (curr_dir - 1).rem_euclid(4),
+            Turn::R => curr_dir = (curr_dir + 1).rem_euclid(4),
+            _ => ()
+        }
+    }
+    println!("Part B: {}", (curr_loc.0 + 1) * 1000 + (curr_loc.1 + 1) * 4 + curr_dir as usize);
 }
 
-fn cube_stuff(grid: &Vec<Vec<char>>, start: &(usize, usize), num: &usize, dir: &i8,
+fn cube_stuff(grid: &Vec<Vec<char>>, start: &(usize, usize), dir: &i8,
               rows: &Vec<Dimension>, cols: &Vec<Dimension>) -> ((usize, usize), i8) {
+    // going from back to bottom
+    if (start.0 == 0) && (start.1 < 100) {
+        return ((start.1 + 100, 0), 0);
+    }
 
+    // bottom to back (the reverse)
+    if (start.0 > 149) && (start.1 == 0) {
+        return ((0, start.0 - 100), 1);
+    }
 
+    // back to left
+    else if (start.1 == 50) && (start.0 < 50) {
+        return ((149 - start.0, 0), 0);
+    }
 
-    ((0, 0), 0)
+    // left to back (reverse)
+    else if (start.1 == 0) && (start.0 < 150) {
+        return ((149 - start.0, 50), 0);
+    }
+
+    // right to bottom (only other case row is 0)
+    else if start.0 == 0 {
+        return ((199, start.1 - 100), 3);
+    }
+
+    // bottom to right (reverse)
+    else if start.0 == 199 {
+        return ((0, start.1 + 100), 1);
+    }
+
+    // right to front (only case col is 149)
+    else if start.1 == 149 {
+        return ((149 - start.0, 99), 2);
+    }
+
+    // front to right (reverse)
+    else if (start.1 == 99) && (start.0 > 99) {
+        return ((149 - start.0, 149), 2);
+    }
+
+    // right to top (only place this function will be called with row = 49)
+    else if start.0 == 49 {
+        return ((start.1 - 50, 99), 2);
+    }
+
+    // top to right (reverse, only other place it'll be 99)
+    else if start.1 == 99 {
+        return ((49, start.0 + 50), 3);
+    }
+
+    // top to left (only other place col is 50)
+    else if start.1 == 50 {
+        return ((100, start.0 - 50), 1);
+    }
+
+    // left to top (reverse, only case where starting row is 100))
+    else if start.0 == 100 {
+        return ((start.1 + 50, 50), 0);
+    }
+
+    // front to bottom (only place row is 149)
+    else if start.0 == 149 {
+        return ((start.1 + 100, 49), 2);
+    }
+
+    // bottom to front (final option)
+    else if start.1 == 49 {
+        return ((149, start.0 - 100), 3);
+    }
+
+    panic!("We shouldn't have reached this point!")
 }
